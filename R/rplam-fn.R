4f4d96940ebd83592a687e2f79282326bf94b030
alemermartinez <ale_m_martinez@hotmail.com>
2025-01-15 18:45:22 -0300

    Description for select.nknots.rob.am

--- R/rplam-fn.R
+++ R/rplam-fn.R
@@ -37,7 +37,7 @@ psi.tukey <- function(r, k=4.685){
 #' @param x A real vector.
 #' @param k tuning constant.
 #'
-#' @return A vector of the same length as r which corresponds to the derivatives of the Tukey's loss function.
+#' @return A vector of the same length as x which corresponds to the derivatives of the Tukey's loss function.
 #'
 #' @author Alejandra Mercedes Martinez, \email{ammartinez@conicet.gov.ar}, Alejandra Martinez
 #'
@@ -46,28 +46,57 @@ psi.tukey <- function(r, k=4.685){
 #' psi.tukey.derivative(x)
 #'
 #' @export
-psi.tukey.derivative <- function(x, k=4.685){
+psi.tukey.derivative <- function(x, k = 4.685){
   u <- abs(x/k)
   w <- ((1-u)*(1+u))^2-4*u^2*((1-u)*(1+u))
   w[u>1] <- 0
   return(w)
 }
 
-#' Tukey Loss Function
+#' Tukey's loss function
+#'
+#' This function computes the Tukey's loss function.
+#'
+#' @param x A real vector.
+#' @param k tuning constant.
+#'
+#' @return A vector of the same length as x which corresponds to values obtained by the Tukey's loss function.
+#'
+#' @author Alejandra Mercedes Martinez, \email{ammartinez@conicet.gov.ar}, Alejandra Martinez
+#'
+#' @examples
+#' x <- seq(-2, 2, length=10)
+#' tukey.loss(x)
+#'
 #' @export
-tukey.loss <- function(x,k=4.685){
+tukey.loss <- function(x, k = 4.685){
   n <- length(x)
   salida <- rep(0,n)
   for(i in 1:n){
-    if(abs(x[i])<=k){salida[i] <- 1-(1-(x[i]/k)^2)^3 #(x[i])^6/(6*k^4)-(x[i])^4/(2*k^2)+(x[i])^2/2
+    if(abs(x[i])<=k){salida[i] <- 1-(1-(x[i]/k)^2)^3
     }else{
-      salida[i] <- 1 #k^2/6
+      salida[i] <- 1
     }
   }
   return(salida)
 }
 
-# Tukey's weight function "Psi(r)/r"
+#' Tukey's weight function
+#'
+#' This function calculates the Tukey's weight function of the form "Psi(r)/r".
+#'
+#' @param r A real vector.
+#' @param k tuning constant.
+#'
+#' @return A vector of the same length as r which corresponds to the Tukey's weight function.
+#'
+#' @author Alejandra Mercedes Martinez, \email{ammartinez@conicet.gov.ar}, Alejandra Martinez
+#'
+#' @examples
+#' r <- seq(-2, 2, length=10)
+#' psi.w(r)
+#'
+#' @export
 psi.w <- function(r, k= 4.685){
   u <- abs(r/k)
   w <- ((1 + u) * (1 - u))^2
@@ -492,11 +521,40 @@ select.nknots.rob <- function(y, Z, X, degree.spline = 3, maxit = 100){
 }
 
 
-#' Robust knot selection for plam
+#' Selection of the number of knots for the robust estimator for additive models
+#'
+#' This function automatically selects the number of internal knots for the B-spline approximation. It uses the BIC criterion with the Tukey's loss function.
+#'
+#' @param y a vector of real numbers.
+#' @param X a matrix of numbers corresponding to the covariates entering in the additive component of the model.
+#' @param degree.spline spline degree. Defaults to \code{'3'}.
+#' @param maxit maximum number of iterations for computing the S- and the MM-regression estimators.
+#'
+#' @return A list with the following components:
+#' \item{nknots}{Number of internal knots selected by the procedure.}
+#' \item{grid.nknots}{Grid of internal knots used.}
+#' \item{BIC}{Values of the BIC criterion for each element of the grid.}
+#' \item{kj}{Number of elements of the B-spline basis used to approximate each additive function. It is calculated as \code{nknots + degree.spline}.}
+#' \item{nbasis}{Total number of elements of the basis of B-splines. This corresponds to \code{d* kj} where \code{d} is the number of covariates entering in the additive part.
+#'
+#' @references
+#' Boente G. and Martinez A. (2023). A robust spline approach in partially linear additive models. Computational Statistics and Data Analysis, 178, 107611.
+#'
+#' \author Alejandra Martinez, \email{ammartinez@conicet.gov.ar}
+#'
 #' @examples
-#' x <- seq(-2, 2, length=10)
+#' set.seed(11)
+#' n <- 100
+#' x1 <- runif(n,-1,1)
+#' x2 <- runif(n,-1,1)
+#' err <- rnorm(n, 0, 0.1)
+#' regre <- 2+x1^3+2*sin(pi*x2)
+#' y <- regre + err
+#' X <- cbind(x1,x2)
+#' sal <- select.nknots.rob.am(y, X)
+#'
 #' @export
-select.nknots.rob.am <- function(y, X, degree.spline=3, maxit=100){
+select.nknots.rob.am <- function(y, X, degree.spline = 3, maxit = 100){
 
   n <- length(y)
   d <- dim(X)[2]
@@ -504,7 +562,7 @@ select.nknots.rob.am <- function(y, X, degree.spline=3, maxit=100){
 
   r <- degree.spline-1
   if(r<1){
-    cat("No se cumple la hipótesis de: 1<=r<=ell-2")
+    cat("Assumption 1<=r not fulfilled")
   }
 
   lim.inf.kj <- ceiling(max(n^(1/(2*r+1))/2, degree.spline+1))
@@ -530,15 +588,13 @@ select.nknots.rob.am <- function(y, X, degree.spline=3, maxit=100){
         nodos.spl <- c(min(X[,ell]), max(X[,ell]))
       }
 
-      #Mat.X[[ell]] <- splines::bs( X[,ell], knots=knots, degree=degree.spline, intercept=FALSE)
       base.beta   <- fda::create.bspline.basis(rangeval = c(min(X[,ell]), max(X[,ell])),
                                           norder = (degree.spline+1),
                                           breaks = nodos.spl)
       aux <- fda::getbasismatrix(X[,ell], base.beta)
       naux <- dim(aux)[2]
-      #Mat.X[[ell]] <- aux-t(matrix(colMeans(aux),naux,n))
 
-      #Centrado con la integral
+      #Centered with the integral
       spl.center   <- fda::getbasismatrix(grilla.tes, base.beta)
       spl.final <- aux
       for (j in 1:naux){
@@ -549,24 +605,23 @@ select.nknots.rob.am <- function(y, X, degree.spline=3, maxit=100){
 
       Xspline <- cbind(Xspline,Mat.X[[ell]])
     }
-    nMat <- dim(Mat.X[[1]])[2] #Decía ell
-    #dim(Xspline)[2]/4
+    nMat <- dim(Mat.X[[1]])[2]
 
 
 
     #- Tukey MM estimator -#
-    control <- robustbase::lmrob.control(trace.level = 0,         # 0
-                             nResample   =  500,      # 500 default
-                             tuning.psi = 4.685061,      # para 85% eff usar 3.443689 # para 95% eff usar 4.685061
-                             subsampling = 'simple',  #
-                             rel.tol     = 1e-5,      # 1e-7
-                             refine.tol  = 1e-5,      # 1e-7
-                             k.max       = 2e3,       # 200
-                             maxit.scale = maxit,       # 200 #2e3
-                             max.it      = maxit)       # 50 #2e3
+    control <- robustbase::lmrob.control(trace.level = 0,
+                             nResample   =  500,
+                             tuning.psi = 4.685061,
+                             subsampling = 'simple',
+                             rel.tol     = 1e-5,
+                             refine.tol  = 1e-5,
+                             k.max       = 2e3,
+                             maxit.scale = maxit,
+                             max.it      = maxit)
     tmp <- try(robustbase::lmrob(y ~ Xspline, control = control))
-    #sal.r  <- robustbase::lmrob(y ~ Z.aux+Xspline, control = control)
-    if( class(tmp)[1] != 'try-error'){ #Comentar esto si no funciona y sacar la def de tmp
+
+    if( class(tmp)[1] != 'try-error'){
       sal.r <- tmp
       betas <- as.vector(sal.r$coefficients)
       beta.hat <- betas[-1]
@@ -576,14 +631,12 @@ select.nknots.rob.am <- function(y, X, degree.spline=3, maxit=100){
 
       gs.hat <- matrix(0,n,d)
       for(ell in 1:d){
-        #aux <- as.vector( Xspline[,(nMat*(ell-1)+1):(nMat*ell)] %*% coef.spl[(nMat*(ell-1)+1):(nMat*ell)] )
-        #gs.hat[,ell] <- aux #- mean(aux) No se necesita porque están centrados con la integral
         gs.hat[,ell] <- as.vector( Xspline[,(nMat*(ell-1)+1):(nMat*ell)] %*% coef.spl[(nMat*(ell-1)+1):(nMat*ell)] )
       }
 
-      regresion.hat.r <- stats::predict(sal.r) #alpha.hat + dummies%*%coef.lin + Xspline%*%coef.spl
+      regresion.hat.r <- stats::predict(sal.r)
 
-      nbasis <- d*(nknots + degree.spline) #d*(nknots + degree.spline + 1)
+      nbasis <- d*(nknots + degree.spline)
       desvio.hat <- sal.r$s
       tuk <- tukey.loss( (y - regresion.hat.r)/desvio.hat )
       RBIC[nknots-lim.inf.nknots+1] <- log( (desvio.hat^2)*sum(tuk) )+ (log(n)/(2*n))*(nbasis+q+1) #q+1 porque q de la parte lineal y 1 de la constante. O sea, q+1 es la cantidad de lineales.
@@ -599,9 +652,8 @@ select.nknots.rob.am <- function(y, X, degree.spline=3, maxit=100){
   kj <- nknots + degree.spline
 
   salida <- list(nknots=nknots, RBIC=RBIC, grid.nknots=grid.nknots, nbasis = nbasis, kj = kj)
+
   return(salida)
-
-
 }
 
 
